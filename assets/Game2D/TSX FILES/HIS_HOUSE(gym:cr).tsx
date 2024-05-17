379c66f3c8f6b5a5b333226a83f83047a598b540
JordanPooleEffect <zmtabinas@gmail.com>
2024-05-17 09:05:37 +0800

    fixed layout: his_house

---
+++ assets/Game2D/TSX FILES/HIS_HOUSE(gym:cr).tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="HIS_HOUSE(gym:cr)" tilewidth="32" tileheight="32" tilecount="560" columns="40">
+ <image source="../(IMAGES) TILES/HIS_HOUSE(gym:cr).png" width="1280" height="448"/>
+</tileset>
