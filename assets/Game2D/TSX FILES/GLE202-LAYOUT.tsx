3435aa8fb19c642f96e561d14d4023de35fe08a4
Kenshi123 <zmtabinas@gmail.com>
2024-05-26 09:53:10 +0800

    improved: NGE_HALL

---
+++ assets/Game2D/TSX FILES/GLE202-LAYOUT.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="GLE202-LAYOUT" tilewidth="32" tileheight="32" tilecount="441" columns="21">
+ <image source="../Maps/GLE202/GLE202-LAYOUT.png" width="672" height="672"/>
+</tileset>
