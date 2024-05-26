a6c369d6eba0ebec3869136f3652331c4dfe6c69
Kenshi123 <zmtabinas@gmail.com>
2024-05-26 10:20:54 +0800

    changed nge_hall, nge_cr

---
+++ assets/Game2D/TSX FILES/final-layout.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="final-layout" tilewidth="32" tileheight="32" tilecount="5400" columns="90">
+ <image source="../Maps/NGE_HALL/final-layout.png" width="2880" height="1920"/>
+</tileset>
