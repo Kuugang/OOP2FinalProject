2276a31a54378c9f74e2882e462be8701ca50a3b
felicity <felicityjcksn@gmail.com>
2024-05-30 00:30:03 +0800

    Exterior

---
+++ assets/Game2D/EXTERIOR/pole red.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="pole red" tilewidth="32" tileheight="32" tilecount="2" columns="1">
+ <image source="445984760_1820492501750814_389324630638496379_n.png" width="44" height="72"/>
+</tileset>
