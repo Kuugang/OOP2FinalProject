5c128708115fb1f542487f924e83a45e0c213e7c
Destiny <jameskennethacabal@gmail.com>
2024-05-30 05:03:30 +0800

    Changes: -Added NPC NGE_HALL

--- core/src/com/mygdx/game/Game2D/World/Maps/NGE_HALL.java
+++ core/src/com/mygdx/game/Game2D/World/Maps/NGE_HALL.java
@@ -1,5 +1,9 @@
 package com.mygdx.game.Game2D.World.Maps;
 
+import com.badlogic.gdx.math.Vector2;
+import com.mygdx.game.Game2D.Entities.NPC.NPC1;
+import com.mygdx.game.Game2D.Manager.ResourceManager;
+import com.mygdx.game.Game2D.Utils.GameQueue;
 import com.mygdx.game.Game2D.World.GameMap;
 
 public class NGE_HALL extends GameMap {
@@ -9,6 +13,14 @@ public class NGE_HALL extends GameMap {
 
     @Override
     public void setNPCS() {
+        for(int i = 0; i < 10; i++){
+            NPC1 npc = new NPC1((i % 2 == 0 ? 100 : 200));
+            npc.setTextureAtlas(ResourceManager.getRandomTA_NPC());
+            npc.setDialogues(ResourceManager.getRandomDialogues());
+            npc.setPosition(new Vector2((i % 2 == 0 ? 33 : 35), 32 - i));
+            npcManager.addNPC(npc);
 
+            GameQueue.add(() -> bodies.add(npc.boxBody));
+        }
     }
 }
