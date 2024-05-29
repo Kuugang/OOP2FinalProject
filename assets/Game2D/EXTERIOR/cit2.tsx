f91a42685884d4e738530f8ca3a87e456fd416ab
felicity <felicityjcksn@gmail.com>
2024-05-30 02:06:19 +0800

    Exterior collisions

--- assets/Game2D/EXTERIOR/cit2.tsx
+++ assets/Game2D/EXTERIOR/cit2.tsx
@@ -1,4 +1,4 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.10" tiledversion="1.10.2" name="cit2" tilewidth="32" tileheight="32" tilecount="75" columns="25">
- <image source="../../Downloads/CEBU LOGO2.png" width="809" height="108"/>
+ <image source="CEBU LOGO2.png" width="809" height="108"/>
 </tileset>
---
+++ assets/Game2D/TSX FILES/GLE_F.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.2" name="GLE_F" tilewidth="32" tileheight="32" tilecount="1200" columns="40">
+ <image source="../EXTERIOR/GLE_F/GLE_F.png" width="1280" height="960"/>
+</tileset>
